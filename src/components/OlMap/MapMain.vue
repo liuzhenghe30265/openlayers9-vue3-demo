f175b57a62028d5797b707cfe454ad3b628aa6a0
liuzhenghe30265 <15901450207@163.com>
2024-07-22 14:46:22 +0800

    fix: 、优化

--- src/components/OlMap/MapMain.vue
+++ src/components/OlMap/MapMain.vue
@@ -2,16 +2,6 @@
 import * as turf from '@turf/turf'
 import { onMounted, type Ref, ref, shallowRef, getCurrentInstance, provide } from 'vue'
 import { omap, type IMap } from '@/utils/map'
-import { useCounterStore } from '@/stores/counter'
-import type BaseLayer from 'ol/layer/Base'
-import OlFeature from 'ol/Feature'
-import OlGeomPoint from 'ol/geom/Point'
-import OlStyleStyle from 'ol/style/Style'
-import OlStyleIcon from 'ol/style/Icon'
-import OlStyleText from 'ol/style/Text'
-import OlStyleFill from 'ol/style/Fill'
-import OlStyleStroke from 'ol/style/Stroke'
-import { Fill, RegularShape, Stroke, Style, Circle as CircleStyle, } from 'ol/style.js'
 
 const mapInstance: any = shallowRef(null) // map 实例
 const map: Ref<IMap> = shallowRef(null) // map 对象
@@ -24,121 +14,26 @@ onMounted(() => {
 
 // 底图切换
 const handleChangeBaseLayer = (id: number) => {
-    const layers: BaseLayer[] | undefined = map.value?.getLayers().getArray()
-    const _layers = layers?.filter(_ => _.get('type') === 'base')
-    _layers?.map(_ => _.setVisible(false))
-    const index: number | undefined = _layers?.findIndex(_ => _.get('id') === id)
-    if (typeof index === 'number' && index > -1) {
-        if (_layers && _layers.length) {
-            const targetLayer = _layers[index]
-            targetLayer.setVisible(true)
-            currentBaseLayer.value = id
-        }
-    }
+    mapInstance.value.changeBaseLayer(id)
+    currentBaseLayer.value = id
 }
 
-// 标注
-// import icon from '@/assets/images/ico01.png'
-const stroke = new Stroke({ color: 'black', width: 1 })
-// const fill = new Fill({ color: 'red' })
-const setSymbolStyle = (index: number, type: string, fill: OlStyleFill) => {
-    const Styles = []
-    if (type === 'RegularShape') {
-        Styles.push(
-            new OlStyleStyle({
-                // 设置图标
-                // image: new OlStyleIcon({
-                //     src: icon,
-                //     anchor: [0.5, 1],
-                //     scale: 0.4
-                // }),
-                // https://openlayers.org/en/latest/examples/regularshape.html
-                image: new RegularShape({
-                    fill: fill,
-                    stroke: stroke,
-                    points: 3,
-                    radius: 6,
-                    rotation: Math.PI / 4,
-                    angle: Math.floor(Math.random() * (359)) + 1,
-                })
-                // 设置图片下面显示字体的样式和内容
-                // text: new OlStyleText({
-                //     font: '14px sans-serif', // 设置字体
-                //     maxAngle: 30,
-                //     offsetX: 10, // 设置文字偏移量
-                //     offsetY: 10,
-                //     text: text, // 文字描述
-                //     fill: new OlStyleFill({
-                //         // 字体颜色
-                //         color: '#000'
-                //     }),
-                //     stroke: new OlStyleStroke({
-                //         // 文字描边
-                //         color: '#fff',
-                //         width: 5
-                //     })
-                // })
-            })
-        )
-    } else if (type === 'CircleStyle') {
-        Styles.push(
-            new OlStyleStyle({
-                image: new CircleStyle({
-                    radius: 6, // 圆的半径
-                    fill: new Fill({
-                        color: 'red', // 填充颜色
-                    }),
-                    stroke: new Stroke({
-                        color: 'black', // 圆边框颜色
-                        width: 1,
-                    }),
-                })
-            })
-        )
-    }
-
-    return Styles
-}
 const handleMark = () => {
-    const layer = mapInstance.value.getLayerById('CustomSymbols')
-    // const layer = getLayerById('CustomSymbols')
     marked.value = !marked.value
     if (marked.value) {
-        layer?.getSource().clear()
+        mapInstance.value.addCustomSymbols('CustomSymbols', [])
         return
     }
-    const view = map.value?.getView()
-    const bbox = view?.calculateExtent(map.value?.getSize())
+    // 随机生成点位
+    const extent = mapInstance.value.getExtent()
     const positions = turf
         .randomPoint(1000, {
-            bbox: bbox
+            bbox: extent
         })
         .features.map(_ => {
             return _.geometry.coordinates
         })
-
-    layer?.getSource().clear()
-
-    const features = []
-    for (let index = 0; index < positions.length; index++) {
-        const point = positions[index]
-        const feature = new OlFeature({
-            geometry: new OlGeomPoint(point),
-            data: {
-                index,
-                point
-            } // 数据存储到 feature 中用来做点击事件
-        })
-        if (index % 2 === 0) {
-            feature.setStyle(setSymbolStyle(index, 'RegularShape', new Fill({ color: 'white' })))
-        } else if (index % 7 === 0) {
-            feature.setStyle(setSymbolStyle(index, 'CircleStyle', new Fill({ color: 'red' })))
-        } else {
-            feature.setStyle(setSymbolStyle(index, 'RegularShape', new Fill({ color: 'red' })))
-        }
-        features.push(feature)
-    }
-    layer?.getSource().addFeatures(features)
+    mapInstance.value.addCustomSymbols('CustomSymbols', positions)
 }
 
 // 初始化地图
@@ -158,22 +53,15 @@ const initMap = () => {
     console.log('getAllLayers', mapInstance.value.getAllLayers())
 }
 
-const counter = useCounterStore()
-const handleClick = () => {
-    // counter.$patch({ count: counter.count + 1 })
-    counter.increment()
-}
 </script>
 
 <template>
     <div id="ol-map" class="ol-map">
-        <!-- <h1 style="position: absolute; bottom: 50px; right: 50px; color: #fff;z-index: 999;" @click="handleClick">
-            {{ counter.count }}</h1> -->
         <div class="nav_control">
             <div class="btn" @click="handleChangeBaseLayer(1)" :class="{ active: currentBaseLayer === 1 }">矢量地图</div>
             <div class="btn" @click="handleChangeBaseLayer(2)" :class="{ active: currentBaseLayer === 2 }">卫星影像图</div>
             <hr>
-            <div class="btn" @click="handleMark">水上目标</div>
+            <div class="btn" @click="handleMark" :class="{ active: !marked }">水上目标</div>
         </div>
     </div>
 </template>
--- src/utils/map/index.ts
+++ src/utils/map/index.ts
@@ -14,6 +14,14 @@ import {
     ZoomSlider,
     FullScreen
 } from 'ol/control'
+import OlStyleStyle from 'ol/style/Style'
+import OlStyleIcon from 'ol/style/Icon'
+import OlStyleText from 'ol/style/Text'
+import OlStyleFill from 'ol/style/Fill'
+import OlStyleStroke from 'ol/style/Stroke'
+import OlFeature from 'ol/Feature'
+import OlGeomPoint from 'ol/geom/Point'
+import { Fill, RegularShape, Stroke, Style, Circle as CircleStyle, } from 'ol/style.js'
 
 export type IMap = Map | null
 
@@ -27,6 +35,11 @@ class OMap {
     }
     getMap() {
         return this.map
+    }
+    getExtent() {
+        const view = this.map?.getView()
+        const extent = view?.calculateExtent(this.map?.getSize())
+        return extent
     }
     // 获取所有图层
     getAllLayers() {
@@ -47,8 +60,105 @@ class OMap {
         })
         this.map?.addLayer(vectorLayer)
     }
-    addOlFeature() {
-        
+    // 底图切换
+    changeBaseLayer(id: number) {
+        const layers = this.getAllLayers()
+        const _layers = layers?.filter(_ => _.get('type') === 'base')
+        _layers?.map(_ => _.setVisible(false))
+        const index: number | undefined = _layers?.findIndex(_ => _.get('id') === id)
+        if (typeof index === 'number' && index > -1) {
+            if (_layers && _layers.length) {
+                const targetLayer = _layers[index]
+                targetLayer.setVisible(true)
+            }
+        }
+    }
+
+    // 添加自定义标注
+    addCustomSymbols(layerId: string, array: any) {
+        if (!layerId) return
+        const layer = this.getLayerById('CustomSymbols')
+        layer?.getSource().clear()
+        if (array && array.length > 0) {
+            const features = []
+            for (let index = 0; index < array.length; index++) {
+                const point = array[index]
+                const feature = new OlFeature({
+                    geometry: new OlGeomPoint(point),
+                    data: {
+                        index,
+                        point
+                    } // 数据存储到 feature 中用来做点击事件
+                })
+                if (index % 2 === 0) {
+                    feature.setStyle(this.setSymbolStyle(index, 'RegularShape', new Fill({ color: 'white' })))
+                } else if (index % 7 === 0) {
+                    feature.setStyle(this.setSymbolStyle(index, 'CircleStyle', new Fill({ color: 'red' })))
+                } else {
+                    feature.setStyle(this.setSymbolStyle(index, 'RegularShape', new Fill({ color: 'red' })))
+                }
+                features.push(feature)
+            }
+            layer?.getSource().addFeatures(features)
+        }
+    }
+    setSymbolStyle(index: number, type: string, fill: OlStyleFill) {
+        const Styles = []
+        if (type === 'RegularShape') {
+            Styles.push(
+                new OlStyleStyle({
+                    // 设置图标
+                    // image: new OlStyleIcon({
+                    //     src: icon,
+                    //     anchor: [0.5, 1],
+                    //     scale: 0.4
+                    // }),
+                    // https://openlayers.org/en/latest/examples/regularshape.html
+                    image: new RegularShape({
+                        fill: fill,
+                        stroke: new Stroke({ color: 'black', width: 1 }),
+                        points: 3,
+                        radius: 6,
+                        rotation: Math.PI / 4,
+                        angle: Math.floor(Math.random() * (359)) + 1,
+                    })
+                    // 设置图片下面显示字体的样式和内容
+                    // text: new OlStyleText({
+                    //     font: '14px sans-serif', // 设置字体
+                    //     maxAngle: 30,
+                    //     offsetX: 10, // 设置文字偏移量
+                    //     offsetY: 10,
+                    //     text: text, // 文字描述
+                    //     fill: new OlStyleFill({
+                    //         // 字体颜色
+                    //         color: '#000'
+                    //     }),
+                    //     stroke: new OlStyleStroke({
+                    //         // 文字描边
+                    //         color: '#fff',
+                    //         width: 5
+                    //     })
+                    // })
+                })
+            )
+        } else if (type === 'CircleStyle') {
+            Styles.push(
+                new OlStyleStyle({
+                    image: new CircleStyle({
+                        radius: 6, // 圆的半径
+                        fill: new Fill({
+                            color: 'red', // 填充颜色
+                        }),
+                        stroke: new Stroke({
+                            color: 'black', // 圆边框颜色
+                            width: 1,
+                        }),
+                    })
+                })
+            )
+        }
+
+        return Styles
     }
     // 添加地图事件
     addEventHandler() {
